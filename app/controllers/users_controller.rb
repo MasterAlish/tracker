6fef9f1315a13caf4308ce6657fe212248c8ed85
sunloverz <sunloverz.kg@gmail.com>
2013-04-30 14:56:39 +0600

    Format code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    authorize! :index, current_user, :message => 'Not authorized as an administrator.'
+    authorize! :index, current_user,
+               :message => 'Not authorized as an administrator.'
   end
 end
