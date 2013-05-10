164ccbdfc634f7fdca2063425c037af8fa07c556
aalikulov <masteraalish@gmail.com>
2013-05-10 17:13:23 +0600

    FIX2:mailer host added to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,21 @@ Tracker::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'glacial-earth-6350.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'glacial-earth-6350.herokuapp.com' }
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'gmail.com',
+      :user_name => ENV['GMAIL_USERNAME'],
+      :password => ENV['GMAIL_PASSWORD'],
+      :authentication => 'plain',
+      :enable_starttls_auto => true
+  }
   # Enable threaded mode
   # config.threadsafe!
 
